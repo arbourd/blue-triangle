6d6e2249f88c87ab0345338c7f2617fe8fba3eea
Dylan Arbour <dylan.arbour@gmail.com>
2016-03-20 18:34:42 -0400

    Add Member/devise models and views

--- db/migrate/20160320211411_create_courses.rb
+++ db/migrate/20160320211411_create_courses.rb
@@ -1,9 +1,9 @@
 class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
-      t.string :name
-      t.boolean :active
-      t.integer :slots
+      t.string :name, null: false
+      t.boolean :active, null: false, default: true
+      t.integer :slots, null: false
 
       t.timestamps null: false
     end
