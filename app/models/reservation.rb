aba87521aa317adfc37a133d0612cd8fdd1e63f0
Dylan Arbour <dylan.arbour@gmail.com>
2016-03-21 22:51:32 -0400

    Add relationships and seed data

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,4 @@
 class Reservation < ActiveRecord::Base
+  belongs_to :course
+  belongs_to :member
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Course.create([{ name: 'Test Course 1', active: true, slots: 8 },
+               { name: 'Test Course 2', active: true, slots: 12 },
+               { name: 'Test Course 3', active: false, slots: 6 }])
+
+Member.create!(
+  [
+    { email: 'test1@eg.ca',
+      password: 'golfer11',
+      password_confirmation: 'golfer11',
+      fname: 'Test',
+      lname: 'Account 1',
+      number: 1222
+    },
+    { email: 'test2@eg.ca',
+      password: 'golfer12',
+      password_confirmation: 'golfer12',
+      fname: 'Test',
+      lname: 'Account 2',
+      number: 1234
+    },
+    { email: 'test3@eg.ca',
+      password: 'golfer13',
+      password_confirmation: 'golfer13',
+      fname: 'Test',
+      lname: 'Account 3',
+      number: 1235
+    }
+  ]
+)
