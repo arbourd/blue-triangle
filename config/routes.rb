71cfddb8a332c01fd94b86674b0b0d12773341a1
nguy0080 <nguy0080@hotmail.com>
2016-03-26 22:27:48 -0400

    imlement master page and calendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   resources :reservations
+  resources :calendar
   devise_for :members
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
