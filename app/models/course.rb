f93841ca58b607778e775c85412466d3d6c11f6d
Dylan Arbour <dylan.arbour@gmail.com>
2016-04-04 12:12:27 -0400

    Add model validation for date range
    Closes #32
    
    Range is hardcoded on the model itself for the time being

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,12 +1,18 @@
 class Course < ActiveRecord::Base
   has_many :reservations
 
+  MAX_DAYS = 60
+
   def available_slots(date)
     raise Exception unless date.is_a?(Date)
     slots - reservations.where(date: date).count
   end
 
-  def reserve?(date)
+  def date_within_max_days?(date)
+    Date.today <= date && date <= Date.today + MAX_DAYS
+  end
+
+  def available?(date)
     return true if available_slots(date) > 0
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,7 @@ class Reservation < ActiveRecord::Base
 
   validate :course_active
   validate :course_available_slots, on: :create
+  validate :course_within_max_days, on: :create
   validate :member_bookings_per_course, on: :create
 
   def course_active
@@ -13,8 +14,18 @@ class Reservation < ActiveRecord::Base
   end
 
   def course_available_slots
-    unless course.reserve?(date)
+    unless course.available?(date)
       errors.add(:errors, "Course '#{course.name}' is fully booked for #{date}")
+    end
+  end
+
+  def course_within_max_days
+    unless course.date_within_max_days?(date)
+      errors.add(
+        :errors,
+        "Cannot book more than #{Course.MAX_DAYS} in advance for " \
+        "'#{course.name}'"
+      )
     end
   end
 
