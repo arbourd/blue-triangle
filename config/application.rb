84ae4ad7bffdf031ed68c2650a93036cd50d1deb
Dylan Arbour <dylan.arbour@gmail.com>
2016-04-05 20:20:53 -0400

    Fix Rails production environment

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'awesome_print'
+require 'awesome_print' unless Rails.env.production?
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
