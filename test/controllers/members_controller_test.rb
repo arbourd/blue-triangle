72ad3d1a5d269ebf523887bdf0b2897f5f0d2889
Dylan Arbour <dylan.arbour@gmail.com>
2016-04-15 17:31:33 -0400

    Add edit profile and password pages
    Add update password functionality
    
    Update password change functions

--- test/controllers/members_controller_test.rb
+++ test/controllers/members_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class MembersControllerTest < ActionController::TestCase
-  test 'should return member name and number' do
+  test 'should return members name and number' do
     sign_in members(:one)
     get :name, number: 1
     assert_response :success
   end
 
-  test 'should return not-found when member is not found' do
+  test 'should return not-found when members is not found' do
     sign_in members(:one)
     get :name, number: -1
     assert_response 404
